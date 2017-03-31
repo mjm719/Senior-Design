053d98a39f2092abe3322d65d06c1cd040438b03
Alden Michaels <am1926@msstate.edu>
2017-03-31 15:04:27 -0500

    BLE update

--- Android/BluetoothLE/MainActivity.cs
+++ Android/BluetoothLE/MainActivity.cs
@@ -1,18 +1,172 @@
 ﻿using Android.App;
-using Android.Widget;
+using Android.Bluetooth;
+using Android.Bluetooth.LE;
+using Android.Content;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Java.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Timers;
 
 namespace BluetoothLE
 {
+    public static class Constants
+    {
+        public const int REQUEST_ENABLE_BT = 1;
+    }
+
     [Activity(Label = "Bluetooth LE Testing", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private LinearLayout layout;
+        public TextView debugText;
+
+        private BluetoothAdapter mBluetoothAdapter;
+        public static BluetoothManager mBluetoothManager;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            layout = new LinearLayout(this);
+            layout.Orientation = Orientation.Vertical;
+            layout.SetGravity(Android.Views.GravityFlags.CenterHorizontal);
+            SetContentView(layout);
+
+            debugText = new TextView(this);
+            layout.AddView(debugText);
+            debugText.Text += "Debugging...\n";
+
+            #region Bluetooth Setup
+
+            // Initializes Bluetooth adapter.
+            mBluetoothManager = (BluetoothManager) GetSystemService(Context.BluetoothService);
+            mBluetoothAdapter = mBluetoothManager.Adapter;
+
+            #region BLE Setup
+
+            // Ensures Bluetooth is available on the device and it is enabled. If not,
+            // displays a dialog requesting user permission to enable Bluetooth.
+
+            if (mBluetoothAdapter == null || !mBluetoothAdapter.IsEnabled)
+            {
+                debugText.Text += "Bluetooth is not enabled on device.\n";
+                debugText.Text += "Requesting permission to enable Bluetooth on device...\n";
+
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ActionRequestEnable);
+                StartActivityForResult(enableBtIntent, Constants.REQUEST_ENABLE_BT);
+            }
+            else if (mBluetoothAdapter.IsEnabled)
+            {
+                debugText.Text += "Bluetooth is enabled on device.\n";
+            }
+
+            #endregion
+
+            #endregion
+
+            #region Load Paired Devices
+
+            debugText.Text += "Loading paired devices...\n";
+
+            ICollection<BluetoothDevice> pairedDevices = mBluetoothAdapter.BondedDevices;
+
+            if (pairedDevices.Count > 0)
+            {
+                // There are pre-existing paired devices.
+                // Get the name and address of each device.
+
+                foreach (BluetoothDevice device in pairedDevices)
+                {
+                    string deviceName = device.Name;
+                    string deviceAddress = device.Address;
+
+                    debugText.Text = debugText.Text + deviceName + " (" + deviceAddress + ")\n";
+                }
+            }
+            else
+            {
+                debugText.Text += "No paired devices were found.\n";
+            }
+
+            #endregion
+
+            #region Find Devices
+
+            // BLE Discovery method has various missing classes/functions, so use the regular
+            // Bluetooth Discovery implementation.
+
+            BluetoothDeviceReceiver mReceiver = new BluetoothDeviceReceiver();
+            mReceiver.debugText = debugText;
+            mReceiver.mBluetoothAdapter = mBluetoothAdapter;
+
+            IntentFilter filter_ActionFound = new IntentFilter(BluetoothDevice.ActionFound);
+            IntentFilter filter_ActionDiscoveryStarted = new IntentFilter(BluetoothAdapter.ActionDiscoveryStarted);
+            IntentFilter filter_ActionDiscoveryFinished = new IntentFilter(BluetoothAdapter.ActionDiscoveryFinished);
+
+            RegisterReceiver(mReceiver, filter_ActionFound);
+            RegisterReceiver(mReceiver, filter_ActionDiscoveryStarted);
+            RegisterReceiver(mReceiver, filter_ActionDiscoveryFinished);
+
+            // Start searching for devices.
+            debugText.Text += "Searching for discoverable devices...\n";
+            mBluetoothAdapter.StartDiscovery();
+
+            #endregion
+        }
+    }
+
+    public class BluetoothDeviceReceiver : BroadcastReceiver
+    {
+        public TextView debugText { get; set; }
+        public BluetoothAdapter mBluetoothAdapter { get; set; }
+
+        private string btAddress = "CC:78:AB:83:3C:06";
+        private bool found = false;
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            string action = intent.Action;
+
+            if (action == BluetoothDevice.ActionFound)
+            {
+                // Discovery has found a device. Get the BluetoothDevice object and its info from the Intent.
+                BluetoothDevice device = (BluetoothDevice)intent.GetParcelableExtra(BluetoothDevice.ExtraDevice);
+
+                string deviceName = device.Name;
+                string deviceAddress = device.Address;
+
+                if (deviceAddress == btAddress)
+                {
+                    found = true;
+
+                    debugText.Text += "Device was found.\n";
+                    debugText.Text = debugText.Text + deviceName + " (" + deviceAddress + ")\n";
+                    
+                    debugText.Text += "Cancelling discovery...\n";
+                    mBluetoothAdapter.CancelDiscovery();
+                }
+            }
+
+            if (action == BluetoothAdapter.ActionDiscoveryStarted)
+            {
+                debugText.Text += "Discovery started...\n";
+            }
+
+            if (action == BluetoothAdapter.ActionDiscoveryFinished)
+            {
+                debugText.Text += "Discovery finished.\n";
+
+                if (!found)
+                {
+                    debugText.Text += "Device was not found.\n";
+                }
+            }
         }
     }
 }
